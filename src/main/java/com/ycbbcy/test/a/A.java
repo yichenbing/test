f06a7e9bc4fc4f489dcd1f212514e4de075fac9c
张仕风 <shifeng.zhang@nascent.cn>
2019-04-23 14:51:37 +0800

    Initial commit

---
+++ src/main/java/com/ycbbcy/test/a/A.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2017 Nascent 版权所有
+ * Nascent Co. Ltd. All rights reserved.
+ *
+ * This software is the confidential and proprietary
+ * information of Nascent Co. Ltd.
+ * ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only
+ * in accordance with the terms of the contract agreement
+ * you entered into with Nascent Co. Ltd
+ */
+package com.ycbbcy.test.a;
+
+/**
+ * @author zsf
+ * @history 1.0.0 2019/4/23 13:38 created by zsf
+ * @since 2019/4/23 13:38
+ */
+public class A {
+
+    public static void main(String[] args) {
+        String a = "aaaaaaaaa";
+    }
+}
